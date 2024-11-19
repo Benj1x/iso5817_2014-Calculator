34dc11872a5eff39d8dce048cdf1674764ae125e
Benjamin Høj <44369126+Benj1x@users.noreply.github.com>
2024-11-19 15:18:14 +0100

    updates

--- lib/models/form_model.dart
+++ lib/models/form_model.dart
@@ -1,12 +1,12 @@
 class FormModel {
   bool filletWeld;
   int buttWeld;
-  String t;
-  String a;
-  String b;
+  double t;
+  double a;
+  double b;
   bool levelB;
   bool levelC;
   bool levelD;
 
-  FormModel({this.filletWeld = true, this.buttWeld = 0, this.t = '', this.a = '', this.b = '', this.levelB = true, this.levelC = false , this.levelD = false});
+  FormModel({this.filletWeld = true, this.buttWeld = 0, this.t = 0.0, this.a = 0.0, this.b = 0.0, this.levelB = true, this.levelC = false , this.levelD = false});
 }
--- lib/models/grading_model.dart
+++ lib/models/grading_model.dart
@@ -1,15 +1,26 @@
 
+import 'dart:ffi';
+
 import 'package:iso5817_2014/models/form_model.dart';
+
+import '../util/gradingStruct.dart';
+import 'gradingB.dart';
+import 'gradingC.dart';
+import 'gradingD.dart';
 
 class GradingModel
 {
-  final double _plateThickness = 0.0;
-  final double _weldTThickness = 0.0;
-  final double _weldWidth = 0.0;
+  double _plateThickness = 0.0;
+  double _weldTThickness = 0.0;
+  double _weldWidth = 0.0;
+
+  bool _isFilletWeld = true;
 
   bool gradeB = false;
   bool gradeC = false;
   bool gradeD = false;
+
+  
 
   double get plateThickness => _plateThickness;
   double get weldTThickness => _weldTThickness;
@@ -18,18 +29,27 @@ class GradingModel
     return _weldTThickness;
   }
 
-  void setData(FormModel formModel){
-    String test = formModel.t;
+  void setData(FormModel formModel)
+  {
+    _plateThickness = formModel.t;
+    _weldTThickness = formModel.a;
+    _weldWidth = formModel.b;
   }
-  // Gradingstruct calcData(){
-  //   if(gradeB){
-  //     return BGrading(a, t, b, isFilletWeld)
-  //   }
-  //   else if (gradeC){
-  //     return CGrading(a, t, b, isFilletWeld)
-  //   }
-  //   else {
-  //     return DGrading(s, a, t, b, isFilletWeld)
-  //   }
-  // }
+  Gradingstruct calcData()
+  {
+   
+    if(gradeB)
+    {
+      return BGrading(_weldTThickness, _plateThickness, _weldWidth, _isFilletWeld);
+    }
+    else if (gradeC)
+    {
+      return CGrading(0.0, _weldTThickness, _plateThickness, _weldWidth, _isFilletWeld);
+    }
+    else if (gradeC)
+    {
+      return DGrading(0.0, _weldTThickness, _plateThickness, _weldWidth, _isFilletWeld);
+    }
+    return new Gradingstruct();
+  }
 }
